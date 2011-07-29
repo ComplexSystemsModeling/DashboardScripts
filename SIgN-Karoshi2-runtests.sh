fbafb572c942098d35131b09bd8b1d9a41bafba8
alex gouaillard <agouaillard@gmail.com>
2011-07-29 12:02:24 +0800

    ENH: add support for crontab modification.

--- SIgN-Karoshi2-runtests.sh
+++ SIgN-Karoshi2-runtests.sh
@@ -9,3 +9,6 @@ git pull >& git.log
 # Run the scripts
 ctest -S SIgN-Karoshi2-ppc-nightly.cmake -V >& SIgN-Karoshi2-ppc-nightly.log
 
+# Update the crontab for next day
+crontab SIgN-Karoshi2.crontab
+
