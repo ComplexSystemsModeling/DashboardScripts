d29433c3f013931314fd43bc22c3b24cdfaed9a7
alex gouaillard <agouaillard@gmail.com>
2011-07-29 12:19:40 +0800

    ENH: add support for intel 64 bits build.

--- SIgN-Karoshi2-runtests.sh
+++ SIgN-Karoshi2-runtests.sh
@@ -7,8 +7,9 @@ cd /Users/alexgouaillard/DEVEL/GITROOT/DashboardScripts
 git pull >& git.log 
 
 # Run the scripts
-ctest -S SIgN-Karoshi2-ppc-nightly.cmake  -V >& SIgN-Karoshi2-ppc-nightly.log
-ctest -S SIgN-Karoshi2-i386-nightly.cmake -V >& SIgN-Karoshi2-i386-nightly.log
+ctest -S SIgN-Karoshi2-ppc-nightly.cmake    -V >& SIgN-Karoshi2-ppc-nightly.log
+ctest -S SIgN-Karoshi2-i386-nightly.cmake   -V >& SIgN-Karoshi2-i386-nightly.log
+ctest -S SIgN-Karoshi2-x86_64-nightly.cmake -V >& SIgN-Karoshi2-x86_64-nightly.log
 
 # Update the crontab for next day
 crontab SIgN-Karoshi2.crontab
