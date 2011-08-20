b12de0143335641908be8617d581f248d60477a4
Alex Gouaillard <agouaillard@gmail.com>
2011-08-20 22:00:53 +0800

    ENH: use 8 threads, comment out arch not set up yet.

--- CoSMo-Singapore-ChickenRice-ITK-runtests.sh
+++ CoSMo-Singapore-ChickenRice-ITK-runtests.sh
@@ -12,8 +12,8 @@ git pull > git.log
 
 #run scripts
 ctest -S CoSMo-Singapore-ChickenRice-ITK-i386-Wrapping.cmake   -V > i386-Wrapping.log
-ctest -S CoSMo-Singapore-ChickenRice-ITK-x86_64-Wrapping.cmake -V > x86_64-Wrapping.log
-ctest -S CoSMo-Singapore-ChickenRice-ITK-ppc-Wrapping.cmake    -V > ppc-Wrapping.log
+#ctest -S CoSMo-Singapore-ChickenRice-ITK-x86_64-Wrapping.cmake -V > x86_64-Wrapping.log
+#ctest -S CoSMo-Singapore-ChickenRice-ITK-ppc-Wrapping.cmake    -V > ppc-Wrapping.log
 
 #update crontab for the day after is needed
 crontab CoSMo-Singapore-ChickenRice.crontab
